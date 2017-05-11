0050586d2d2e3af986dc17c5c7f10935308fc921
dzenang <dzenan.granulo@gmail.com>
2017-05-12 00:51:05 +0200

    Implementing Content Provider (PetProvider)

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -11,8 +12,16 @@ public final class PetContract {
     // Private constructor to prevent accidental instantiation
     private PetContract() {}
 
+    // Content provider URI constants
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     // Inner class that defines constants for pets table
     public static abstract class PetEntry implements BaseColumns {
+
+        // Content URI to access pets data in the content provider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         // String constant for table name
         public static final String TABLE_NAME = "pets";
